8472a05852c863c03496dafd0b58b01994cade81
ayan4m1 <andrew@bulletlogic.com>
2023-07-09 18:25:11 -0400

    fix flush interval

--- src/modules/flushBucket.js
+++ src/modules/flushBucket.js
@@ -27,7 +27,10 @@ export class FlushBucket {
       return;
     }
 
-    this.taskHandle = setInterval(this.flush, this.flushIntervalSec * 1000);
+    this.taskHandle = setInterval(
+      this.flush,
+      this.flushIntervalSec * 60 * 1000
+    );
     this.running = true;
   }
 
